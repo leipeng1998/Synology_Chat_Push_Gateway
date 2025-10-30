4447dd51982db70a300771849ce8304aca60fa2c
levsc <1021970414@qq.com>
2025-10-30 16:25:20 +0800

    凑合可用

--- syno_func.py
+++ syno_func.py
@@ -1,4 +1,20 @@
 """
+Synology Chat Push Gateway
+Copyright 2024 leipeng1998
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+"""
 群晖消息推送网关核心功能模块
 提供与群晖Chat API的交互、消息监控和推送功能
 """
